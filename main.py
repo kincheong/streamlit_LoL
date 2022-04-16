c826c5f0546f49986f8b11007de685cd151f7dfd
kincheong <jacobkclow@gmail.com>
2022-04-16 14:19:28 +1000

    remove labels

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ st.markdown(
 	unsafe_allow_html=True
 )
 
+# Hide main menu
+hide_menu_style = """
+	<style>
+	#MainMenu {visibility: hidden;}
+	footer {visibility: hidden;}
+	</style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 app = MultiPage()
 
 st.sidebar.header("Menu")
